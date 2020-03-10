f172278bdd426c2ceaf6a2de58af5777d2d89b4a
kick1212 <berry8901@gmail.com>
2020-03-10 20:46:40 +0900

    add 주식.py

---
+++ new_folder/stock_price.py
@@ -0,0 +1,19 @@
+from collections import deque
+
+def solution(prices):
+    answer = []
+    prices = deque(prices)
+
+    while prices:
+        cnt = 0
+        value = prices.popleft()
+        for i in prices:
+            if value > i:
+                cnt += 1
+                break
+            cnt += 1
+        answer.append(cnt)
+    return answer
+
+solution([1,2,3,2,3])
+
